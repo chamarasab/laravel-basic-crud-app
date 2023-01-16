13ed28f909721d022be939579d59ccd4b4b9591b
chamarasab <chamara.sab@outlook.com>
2023-01-16 20:27:50 +0530

    first commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use App\Http\Requests\StoreStudentRequest;
+use App\Http\Requests\UpdateStudentRequest;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$students = Student::all();
+        $students = Student::latest()->paginate(5);
+        return view('students_view',compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreStudentRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreStudentRequest $request)
+    {
+        //validate
+        $validated = $request->validate([
+           'name'=>'required|unique:students',
+            'address'=>'required',
+            'contact'=>'required'
+        ]);
+        //store
+        $student = new Student();
+        $student->name = $request->name;
+        $student->address = $request->address;
+        $student->contact = $request->contact;
+        $student->save();
+        //redirect
+        return redirect()->back()->with('message','Insert Success');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateStudentRequest  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateStudentRequest $request, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        //
+    }
+}
